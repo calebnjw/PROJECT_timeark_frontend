562fadbdfa47813da3b58432bb29b2db07e9d694
nikkirosche <101503635+nikkirosche@users.noreply.github.com>
2022-08-14 15:03:40 +0800

    updated commit

---
+++ src/pages/Invoice.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const InvoicePage = () => {
+    return (
+        <>
+            <div>This is my main landing page</div>
+        </>
+    )
+}
