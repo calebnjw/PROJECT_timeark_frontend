0071f344a456ae19e4b0f50558817560785b3d8f
Goh Yong Siang Ivan <37769720+gysiang@users.noreply.github.com>
2022-08-19 14:21:08 +0800

    stashing changes

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,1 +1,43 @@
-export {};
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+
+const drawerWidth = 240;
+
+export default function Sidebar() {
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width: drawerWidth,
+          boxSizing: "border-box",
+        },
+      }}
+    >
+      <Toolbar />
+      <Box sx={{ overflow: "auto" }}>
+        <List>
+          {["Dashboard", "Time Tracking", "Clients", "Invoices"].map(
+            (text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            )
+          )}
+        </List>
+        <Divider />
+      </Box>
+    </Drawer>
+  );
+}
