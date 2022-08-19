d17eba1972cef83a293edcf9858260652535cab7
Goh Yong Siang Ivan <37769720+gysiang@users.noreply.github.com>
2022-08-19 10:07:23 +0800

    simple frontpage done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.scss";
+import Home from "./pages/home/home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,19 @@
+import BottomNavigation from "@mui/material/BottomNavigation";
+
+export default function Footer() {
+  return (
+    <BottomNavigation
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        align: "center",
+        bgcolor: "primary.main",
+        color: "white",
+      }}
+    >
+      <p>Copyright &copy; 2022 Time Ark</p>
+    </BottomNavigation>
+  );
+}
