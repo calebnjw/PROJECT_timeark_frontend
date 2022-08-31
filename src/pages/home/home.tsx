3b2d5d9b0628d849d49261d8864a2be7bd3438e5
Goh Yong Siang Ivan <37769720+gysiang@users.noreply.github.com>
2022-09-01 04:32:18 +0800

    removed sidebar from home

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,7 +2,6 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Image from "material-ui-image";
 import Typography from "@mui/material/Typography";
-// import freelancerImg from "../assets/images/freelancer.jpg";
 
 import Navbar from "../../components/navbar";
 import Sidebar from "../../components/sidebar";
@@ -15,37 +14,38 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <Sidebar />
-      <div style={{ width: "80%", marginLeft: "20%", marginTop: "80px" }}>
-        <Box sx={{ mt: "2rem" }}>
-          <Typography variant="h3" align="center" mb={"2rem"}>
-            Time Tracking &#38; Invoicing App for Freelancers
-          </Typography>
-          <Grid
-            container
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid item xs={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              id justo eget quam tristique volutpat in non ante. Praesent
-              egestas varius diam, eu volutpat felis malesuada non. Phasellus
-              suscipit risus orci, eget condimentum neque elementum maximus.
-              Curabitur vitae sapien non elit euismod fringilla non a est. Ut
-              pellentesque fermentum imperdiet. Duis fringilla ipsum vitae felis
-              maximus, ac rhoncus neque porta. Curabitur placerat velit at
-              tortor tincidunt, lobortis sollicitudin lectus lacinia. Curabitur
-              eleifend sit amet quam vel rutrum. Phasellus sed mollis quam, eget
-              mollis augue.
-            </Grid>
-            <Grid item xs={1}></Grid>
-            <Grid item xs={2}>
-              {/* <Image src={freelancerImg} /> */}
-            </Grid>
+      {/* <Sidebar /> */}
+
+      <Box
+        style={{
+          marginTop: "80px",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="h3" align="center">
+          Time Tracking &#38; Invoicing App for Freelancers
+        </Typography>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          marginTop="30px"
+        >
+          <br />
+          <Grid item xs={8}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id
+            justo eget quam tristique volutpat in non ante. Praesent egestas
+            varius diam, eu volutpat felis malesuada non. Phasellus suscipit
+            risus orci, eget condimentum neque elementum maximus. Curabitur
+            vitae sapien non elit euismod fringilla non a est. Ut pellentesque
+            fermentum imperdiet. Duis fringilla ipsum vitae felis maximus, ac
+            rhoncus neque porta. Curabitur placerat velit at tortor tincidunt,
+            lobortis sollicitudin lectus lacinia. Curabitur eleifend sit amet
+            quam vel rutrum. Phasellus sed mollis quam, eget mollis augue.
           </Grid>
-        </Box>
-      </div>
+        </Grid>
+      </Box>
     </div>
   );
 }
