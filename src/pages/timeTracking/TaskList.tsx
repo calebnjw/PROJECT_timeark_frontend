9859bddbe86735bd67cf7683ac5e8288159e6b3d
eva.wang <eva.wang81@gmail.com>
2022-09-02 22:51:00 +0800

    added show tasks by selected date

--- src/pages/timeTracking/TaskList.tsx
+++ src/pages/timeTracking/TaskList.tsx
@@ -1,7 +1,9 @@
 import { format } from "date-fns";
 import { useEffect, useState } from "react";
+import { Task, Time } from "../../types/task";
 
 import axios from "axios";
+import el from "date-fns/esm/locale/el/index.js";
 axios.defaults.withCredentials = true;
 
 interface Props {
@@ -10,19 +12,48 @@ interface Props {
 
 const TaskList = (props: Props) => {
   const selectedDate = format(new Date(props.data), "yyyy-MM-dd");
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState<Task[]>([]);
 
   useEffect(() => {
     const getTasksBySelectedDate = async () => {
       const tasks = await axios.get(
-        `${process.env.REACT_APP_BACKEND_URL}/tasks/${selectedDate}`
+        `${process.env.REACT_APP_BACKEND_URL}/tasks/time/${selectedDate}`
       );
-      console.log("selected data task list: ", tasks);
+      const taskArr = tasks.data.tasksBySelectedDate;
+      console.log("selected data task list: ", taskArr);
+      if (taskArr) {
+        setTaskList(taskArr);
+      } else {
+        setTaskList([]);
+      }
     };
     getTasksBySelectedDate();
-  });
+  }, [selectedDate]);
 
-  return <div>Selected Date: {props.data}</div>;
+  if (taskList) {
+    return (
+      <div>
+        <p>Selected Date: {props.data}</p>
+        <div>
+          <ul>
+            {taskList.length &&
+              taskList.map((task, idx) => (
+                <li key={idx}>
+                  Task Name: {task.name} <br /> Hours Spent:{" "}
+                  <ul>
+                    {task.time_tracking.map((time, idx) => (
+                      <li key={idx}>{time.hours}</li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    );
+  } else {
+    return <div>No Task Available</div>;
+  }
 };
 
 export default TaskList;
