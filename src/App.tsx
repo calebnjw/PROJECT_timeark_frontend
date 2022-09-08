c69b5d71c6e2e63ea1f74c9b22448059a14d5ea6
Caleb <5322420+calebnjw@users.noreply.github.com>
2022-09-08 19:52:03 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import "./App.scss";
@@ -26,45 +26,12 @@ import Page404 from "./pages/notFound/Page404";
 import Dashboard from "./pages/dashboard/dashboard";
 import Time from "./pages/timeTracking/time";
 
-import { User } from "./types/user";
-
-import { ClientGlobalContext } from "./context/clientContext";
-import { UserContext } from "./context/userContext";
-
 import axios from "axios";
 import HomeLayout from "./layout/homeLayout";
 import AppLayout from "./layout/appLayout";
 axios.defaults.withCredentials = true;
 
 function App() {
-  const [clientList, setClientList] = useState<[]>([]);
-  const [userProfile, setUserProfile] = useState<User>();
-  const [newUser, setNewUser] = useState<boolean>(false);
-  const [userId, setUserId] = useState<string>(""); // Please replace your user id here!!! DONT FORGET ADD YOUR USER ID TO CLIENT IN DB
-
-  // get user info
-  useEffect(() => {
-    const getProfile = async () => {
-      const result = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/users/user`);
-      setUserProfile(result.data.user);
-      setNewUser(result.data.newUser);
-      setUserId(result.data.user.id);
-    };
-    getProfile();
-  }, []);
-
-  useEffect(() => {
-    if (userProfile) {
-      const getClients = async () => {
-        const result = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/clients`, {
-          params: { user_id: userProfile?._id },
-        });
-        setClientList(result.data);
-      };
-      getClients();
-    }
-  }, [userProfile]);
-
   return (
     <Routes>
       <Route path="/" element={<HomeLayout />}>
@@ -72,49 +39,33 @@ function App() {
         <Route path="login" element={<Login />} />
       </Route>
 
-      <UserContext.Provider
-        value={{
-          userProfile,
-          setUserProfile,
-          newUser,
-        }}
-      >
-        <ClientGlobalContext.Provider
-          value={{
-            clientList,
-            setClientList,
-            userId,
-          }}
-        >
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="onboard" element={<Onboard />} />
+      <Route path="app" element={<AppLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="onboard" element={<Onboard />} />
 
-            <Route path="clients" element={<Clients />} />
-            <Route path="clients/new" element={<AddClient />} />
-            <Route path="clients/:clientId" element={<SingleClient />} />
-            <Route path="clients/:clientId/update" element={<EditSingleClient />} />
+        <Route path="clients" element={<Clients />} />
+        <Route path="clients/new" element={<AddClient />} />
+        <Route path="clients/:clientId" element={<SingleClient />} />
+        <Route path="clients/:clientId/update" element={<EditSingleClient />} />
 
-            <Route path="projects" element={<Projects />} />
-            <Route path="projects/new" element={<NewProject />} />
-            <Route path="projects/:project_id" element={<SingleProject />} />
-            <Route path="projects/:project_id/update" element={<EditProjectForm />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="projects/new" element={<NewProject />} />
+        <Route path="projects/:project_id" element={<SingleProject />} />
+        <Route path="projects/:project_id/update" element={<EditProjectForm />} />
 
-            <Route path="tasks" element={<Tasks />} />
-            <Route path="tasks/new" element={<NewTask />} />
+        <Route path="tasks" element={<Tasks />} />
+        <Route path="tasks/new" element={<NewTask />} />
 
-            <Route path="profile" element={<Profile />}>
-              <Route index element={<ProfileInfo />} />
-              <Route path="edit" element={<ProfileEdit />} />
-            </Route>
+        <Route path="profile" element={<Profile />}>
+          <Route index element={<ProfileInfo />} />
+          <Route path="edit" element={<ProfileEdit />} />
+        </Route>
 
-            <Route path="time" element={<Time />} />
+        <Route path="time" element={<Time />} />
 
-            <Route path="invoices" element={<Invoices />} />
-          </Route>
-        </ClientGlobalContext.Provider>
-      </UserContext.Provider>
+        <Route path="invoices" element={<Invoices />} />
+      </Route>
 
       <Route path="*" element={<Page404 />} />
     </Routes>
--- src/layout/appLayout.tsx
+++ src/layout/appLayout.tsx
@@ -1,30 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Box, Divider, Grid } from "@mui/material";
+
+import { ClientGlobalContext } from "../context/clientContext";
+import { UserContext } from "../context/userContext";
 
 import { useUserContext } from "../context/userContext";
 
 import AppNavbar from "../components/navbar-App";
 import Sidebar from "../components/sidebar";
 
+import { User } from "../types/user";
+
 import axios from "axios";
 import Footer from "../components/footer";
 axios.defaults.withCredentials = true;
 
 function AppLayout() {
-  const { userProfile } = useUserContext();
+  const [clientList, setClientList] = useState<[]>([]);
+  const [userProfile, setUserProfile] = useState<User>();
+  const [newUser, setNewUser] = useState<boolean>(false);
+  const [userId, setUserId] = useState<string>(""); // Please replace your user id here!!! DONT FORGET ADD YOUR USER ID TO CLIENT IN DB
+
+  // get user info
+  useEffect(() => {
+    const getProfile = async () => {
+      const result = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/users/user`);
+      setUserProfile(result.data.user);
+      setNewUser(result.data.newUser);
+      setUserId(result.data.user.id);
+    };
+    getProfile();
+  }, []);
+
+  useEffect(() => {
+    if (userProfile) {
+      const getClients = async () => {
+        const result = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/clients`, {
+          params: { user_id: userProfile?._id },
+        });
+        setClientList(result.data);
+      };
+      getClients();
+    }
+  }, [userProfile]);
 
   return (
-    <Box sx={{ minHeight: "100vh" }}>
-      <AppNavbar />
-      <Grid container direction="row" justifyContent="space-between" alignItems="center">
-        <Grid item>
-          <Sidebar />
-        </Grid>
-        <Outlet />
-      </Grid>
-      <Footer />
-    </Box>
+    <UserContext.Provider
+      value={{
+        userProfile,
+        setUserProfile,
+        newUser,
+      }}
+    >
+      <ClientGlobalContext.Provider
+        value={{
+          clientList,
+          setClientList,
+          userId,
+        }}
+      >
+        <Box sx={{ minHeight: "100vh" }}>
+          <AppNavbar />
+          <Grid container direction="row" justifyContent="space-between" alignItems="center">
+            <Grid item>
+              <Sidebar />
+            </Grid>
+            <Outlet />
+          </Grid>
+          <Footer />
+        </Box>
+      </ClientGlobalContext.Provider>
+    </UserContext.Provider>
   );
 }
 
