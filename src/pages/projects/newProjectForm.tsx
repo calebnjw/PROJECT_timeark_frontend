af228d60de55d735ea6e75ceed728aa98dcd8185
eva.wang <eva.wang81@gmail.com>
2022-09-13 12:22:56 +0800

    keep current code in newProjectForm

--- src/pages/projects/newProjectForm.tsx
+++ src/pages/projects/newProjectForm.tsx
@@ -91,7 +91,12 @@ const NewProjectForm = () => {
               New Project
             </h3>
             {clientOptions.length ? (
-              <TextField select name="client_id" label="*Client" defaultValue={clientOptions[0].id}>
+              <TextField
+                select
+                name="client_id"
+                label="*Client"
+                defaultValue={clientOptions[0].id}
+              >
                 {clientOptions.map((option?: { id: string; name: string }) => (
                   <MenuItem key={option?.id} value={option?.id}>
                     {option?.name}
@@ -102,7 +107,6 @@ const NewProjectForm = () => {
               <>Loading Client Options</>
             )}
 
-<<<<<<< HEAD
             <Button
               color="success"
               variant="contained"
@@ -146,21 +150,6 @@ const NewProjectForm = () => {
                 </Link>
               </Button>
             </div>
-=======
-            <Button color="success" variant="contained" onClick={() => navigate("/clients/new")}>
-              + New Client
-            </Button>
-            <TextField type="text" name="name" label="*Project Name" />
-            <TextField type="number" name="budget" label="*Budget" />
-            <TextField type="number" name="rate" label="*Rate" />
-            <TextField type="date" name="due_date" label="*Due Date" defaultValue={"2022-08-26"} />
-            <TextField name="category_name" label="Category" multiline />
-          </div>
-          <div>
-            <Button variant="contained" color="primary" type="submit" value="Submit">
-              Submit
-            </Button>
->>>>>>> f19b7470919144688d19f335a298967912a42146
           </div>
         </form>
       </Box>
