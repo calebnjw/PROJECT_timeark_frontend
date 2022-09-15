d7e7849b91e68adb3fc0d0f2c404506cf1b68186
Caleb <5322420+calebnjw@users.noreply.github.com>
2022-09-15 10:44:34 +0800

    removed padding

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -20,8 +20,6 @@ const Projects = () => {
       <Container
         style={{
           width: "100%",
-          marginLeft: "23%",
-          marginTop: "100px",
         }}
       >
         <Box sx={{ flexGrow: 1 }}>
@@ -35,16 +33,9 @@ const Projects = () => {
             <Grid item xs={6}>
               <h2>Clients/Projects</h2>
             </Grid>
-            <Grid
-              item
-              xs={6}
-              style={{ textAlign: "right", marginRight: "100px" }}
-            >
+            <Grid item xs={6} style={{ textAlign: "right", marginRight: "100px" }}>
               <Button variant="contained" color="success">
-                <Link
-                  to="new"
-                  style={{ color: "white", textDecoration: "none" }}
-                >
+                <Link to="new" style={{ color: "white", textDecoration: "none" }}>
                   + New Project
                 </Link>
               </Button>
